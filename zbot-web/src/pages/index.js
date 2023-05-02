67de6c1e6eed3fdc1fdf82c1e3087429975a1d2e
Zain Ul Abidin <118212795+Zabidin24@users.noreply.github.com>
2023-05-02 07:49:10 +0000

    Advance step

--- zbot-web/src/pages/index.js
+++ zbot-web/src/pages/index.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 export default function Home() {
-  return (
-   <div> hello world </div>
-  )
-  
+  const [apikey, SetApikey]=useState('');
+
+  return <div className="flex flex-col h-screen"> 
+      <nav className="shadow p-2 flex flex-row justify-between items-center">
+         <div className="text-xl font-bold"> Zbot </div>
+         <div>
+          <input type="password" className="border p-2 rounded"
+          onChange={e=> SetApikey(e.target.value)} 
+          value={apikey}
+          placeholder="paste your key here"/>
+         </div>
+         </nav> 
+         
+         </div>
 }
